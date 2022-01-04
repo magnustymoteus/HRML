95d8bc2c0ea91cde6f5addba36e3b4ac6f6c29a0
magnustymoteus <gruzixpotato@gmail.com>
2022-01-04 01:42:51 +0100

    0.02

--- main.cpp
+++ main.cpp
@@ -6,10 +6,18 @@ protected:
     unsigned int id;
     std::string name;
     bool type=0;
-};
-class OpenTag : public Tag {
-protected:
     std::vector<std::pair<std::string, std::string>> attrs;
+public:
+    void pushAttr(std::pair<std::string, std::string> attrArg) {
+        attrs.insert(std::lower_bound(attrs.begin(), attrs.end(), attrArg), attrArg);
+    }
+    void printAttrs() {
+        std::cout << name << "(ID " << id << ")" << std::endl << "--------------------\n";
+        for(auto i:attrs) {
+            std::cout << i.first << ": " << i.second << std::endl;
+        }
+        std::cout << "--------------------\n";
+    }
 };
 class Parser {
 protected:
@@ -22,11 +30,13 @@ public:
         tag.id = tagsAm;
         (tagStr.substr(0,2)=="</" && tagStr[tagStr.size()-1]=='>')?tag.type=0:(tagStr[0]=='<' && tagStr[tagStr.size()-1]=='>')?tag.type=1,tagsAm++:throw 1;
         if(tag.type) {
+        std::pair<std::string, std::string> attr;
         bool phase=0;
-        for(signed int i=1;i<tagStr.size();i++) {
-            if(!phase)tag.name+=(tagStr[i] == ' ' || tagStr[i] == '>')?phase=1:tagStr[i];
+        for(signed int i=1, c=0;i<tagStr.size();((tagStr[i]=='='||tagStr[i]=='\"') && c<2)?c++,i++:(tagStr[i]=='\"')?c=0,tag.pushAttr(attr),attr={},i++:i++) {
+            if(!phase)tag.name+=(tagStr[i] == ' ' || tagStr[i] == '>')?'\0',phase++:tagStr[i];
             else {
-
+                if(!c && tagStr[i]!=' ' && tagStr[i]!='=')attr.first+=tagStr[i];
+                else if(c==2 && tagStr[i] != '\"' && tagStr[i]!='=') attr.second+=tagStr[i];
             }
         }
         }
@@ -39,7 +49,13 @@ public:
 };
 unsigned int Parser::tagsAm = 0;
 int main() {
+    unsigned int n;
     Parser parser;
     parser.addTag("<tag-1 value = \"some value\" type=\"someType\">");
+    parser.addTag("<tag-2 Sentence=\"A random sentence.\">");
+    /*
+    to do:
+    program closing tag, tags vector, tag query with binary search, nested tags
+    */
     return 0;
 }
