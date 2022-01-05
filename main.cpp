aeaae311432e52cf5a1c520aeeaef442cad8e185
magnustymoteus <gruzixpotato@gmail.com>
2022-01-05 01:40:22 +0100

    0.03

--- main.cpp
+++ main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <vector>
+
 class Tag {
 protected:
     friend class Parser;
     unsigned int id;
     std::string name;
-    bool type=0;
+    bool closed=0;
     std::vector<std::pair<std::string, std::string>> attrs;
+    std::vector<Tag> children;
 public:
-    void pushAttr(std::pair<std::string, std::string> attrArg) {
-        attrs.insert(std::lower_bound(attrs.begin(), attrs.end(), attrArg), attrArg);
+    void insertAttr(std::pair<std::string, std::string> &attrRef) {
+        attrs.insert(std::lower_bound(attrs.begin(), attrs.end(), attrRef), attrRef);
     }
-    void printAttrs() {
+    void outputInfo() {
         std::cout << name << "(ID " << id << ")" << std::endl << "--------------------\n";
         for(auto i:attrs) {
             std::cout << i.first << ": " << i.second << std::endl;
@@ -21,37 +23,53 @@ public:
 };
 class Parser {
 protected:
-    static unsigned int tagsAm;
+    static unsigned int tagsAmount;
     std::vector<Tag> tags;
 public:
+    void closeTag(std::string &tagNameRef) {
+        //to do
+    }
     void addTag(std::string tagStr) {
-        Tag tag;
         try {
-        tag.id = tagsAm;
-        (tagStr.substr(0,2)=="</" && tagStr[tagStr.size()-1]=='>')?tag.type=0:(tagStr[0]=='<' && tagStr[tagStr.size()-1]=='>')?tag.type=1,tagsAm++:throw 1;
-        if(tag.type) {
+        bool type=0;
+        unsigned int id;
+        (tagStr.substr(0,2)=="</" && tagStr[tagStr.size()-1]=='>')?type=0:(tagStr[0]=='<' && tagStr[tagStr.size()-1]=='>')?type=1:throw 1;
+        tagsAmount++;
+        if(type) {
+        Tag tag;
+        tag.id = tagsAmount;
         std::pair<std::string, std::string> attr;
         bool phase=0;
-        for(signed int i=1, c=0;i<tagStr.size();((tagStr[i]=='='||tagStr[i]=='\"') && c<2)?c++,i++:(tagStr[i]=='\"')?c=0,tag.pushAttr(attr),attr={},i++:i++) {
+        for(signed int i=1, c=0;i<tagStr.size();((tagStr[i]=='='||tagStr[i]=='\"') && c<2)?c++,i++:(tagStr[i]=='\"')?c=0,tag.insertAttr(attr),attr={},i++:i++) {
             if(!phase)tag.name+=(tagStr[i] == ' ' || tagStr[i] == '>')?'\0',phase++:tagStr[i];
             else {
                 if(!c && tagStr[i]!=' ' && tagStr[i]!='=')attr.first+=tagStr[i];
                 else if(c==2 && tagStr[i] != '\"' && tagStr[i]!='=') attr.second+=tagStr[i];
+                }
+            }
+            tags.push_back(tag);
+        }
+        else {
+            std::string name;
+            for(unsigned int i=1;i<tagStr.size();i++) {
+                if(tagStr[i] == ' ' || tagStr[i] == '>')name+=tagStr[i];
+            }
+            closeTag(name);
             }
         }
-        }
-        }
+
         catch(int e) {
             std::cout << "Error " << e;
-            (e==1)?std::cout << ": wrong constructor statement for tag with tagId " << tag.id : std::cout << "Unknown error\n";
+            (e==1)?std::cout << ": wrong constructor or closing statement for tag with tagId " << tagsAmount : std::cout << "Unknown error\n";
         }
     }
 };
-unsigned int Parser::tagsAm = 0;
+unsigned int Parser::tagsAmount = 0;
 int main() {
     unsigned int n;
     Parser parser;
     parser.addTag("<tag-1 value = \"some value\" type=\"someType\">");
+    parser.addTag("</tag-1>");
     parser.addTag("<tag-2 Sentence=\"A random sentence.\">");
     /*
     to do:
