2a18c84f9d2cf3371fd32bb8e393512ea09617a0
magnustymoteus <gruzixpotato@gmail.com>
2022-01-08 01:16:46 +0100

    0.7

--- main.cpp
+++ main.cpp
@@ -36,31 +36,23 @@ protected:
     std::vector<Tag> tagLayers;
     signed int currTagIndex = -1;
 
-    signed int bSearchTag(const std::string &tagNameRef, bool opt) {
-        signed int l=0,r=tags.size()-1,p;
+    signed int bSearchTag(std::vector<Tag> &tagVectorRef, const std::string &tagNameRef, bool opt) {
+        signed int l=0,r=tagVectorRef.size()-1,p;
         while(l<=r) {
             p=(l+r)/2;
-            if((tags[p].name==tagNameRef && !opt)) return p;
-            (tagNameRef>tags[p].name)?l=p+1:r=p-1;
+            if((tagVectorRef[p].name==tagNameRef && !opt)) return p;
+            (tagNameRef>tagVectorRef[p].name)?l=p+1:r=p-1;
         }
         return (!opt)?-1:r+1;
     }
 public:
-    void printTags() {
-        for(auto tag : tags) {
-            std::cout << "'" << tag.name << "' ";
-        }
-    }
-    void printChildrenOf(std::string tagNameArg) {
-        Tag tag = tags[bSearchTag(tagNameArg, 0)];
-        for(auto childTag : tag.children) {
-            std::cout << childTag.name << " ";
-        }
-    }
     void insertTag(const Tag &tagRef) {
-        if(tagLayers.size()) tags[bSearchTag(tagLayers[tagLayers.size()-1].name,0)].children.push_back(tagRef);
+        if(tagLayers.size()) {
+            Tag &parentTagRef = tags[bSearchTag(tags, tagLayers[tagLayers.size()-1].name,0)];
+            parentTagRef.children.insert(parentTagRef.children.begin()+bSearchTag(parentTagRef.children, tagRef.name, 1), tagRef);
+        }
         tagLayers.push_back(tagRef);
-        tags.insert(tags.begin()+bSearchTag(tagRef.name, 1), tagRef);
+        tags.insert(tags.begin()+bSearchTag(tags, tagRef.name, 1), tagRef);
     }
     void closeTag() {
         tagLayers.pop_back();
@@ -70,10 +62,9 @@ public:
         bool type=0;
         unsigned int id;
         (tagStr.substr(0,2)=="</" && tagStr[tagStr.size()-1]=='>')?type=0:(tagStr[0]=='<' && tagStr[tagStr.size()-1]=='>')?type=1:throw 1;
-        tagsAmount++;
         if(type) {
         Tag tag;
-        tag.id = tagsAmount;
+        tag.id = tagsAmount++;
         std::pair<std::string, std::string> attr;
         bool phase=0;
         for(signed int i=1, c=0;i<tagStr.size();((tagStr[i]=='='||tagStr[i]=='\"') && c<2)?c++,i++:(tagStr[i]=='\"')?c=0,tag.insertAttr(attr),attr={},i++:i++) {
@@ -103,15 +94,15 @@ unsigned int Parser::tagsAmount = 0;
 int main() {
     Parser parser;
     parser.addTag("<tag-1 value = \"some value\" type=\"someType\">");
-    parser.addTag("<tag-2 Sentence=\"A random sentence.\">");
-    parser.addTag("</tag-2>");
-    parser.addTag("<tag-3 ab = \"a\" bc=\"c\" ba = \"b\">");
+    parser.addTag("<tag-3 Sentence=\"A random sentence.\">");
     parser.addTag("</tag-3>");
+    parser.addTag("<tag-2 attribute = \"someAttribute\" bc=\"c\" ba = \"b\">");
+    parser.addTag("</tag-2>");
     parser.addTag("</tag-1>");
-    parser.printChildrenOf("tag-1");
+    parser.specifyIQ();
     /*
     to do:
-    opening and closing tag same id, tag query system, clean and shorten unnecessary code
+    tag query system, clean and shorten unnecessary code
     */
     return 0;
 }
